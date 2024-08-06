d8d254c38dc64f1520077f8d083a4b25affedfe4
ms0208 <manav.8230680@gmail.com>
2024-08-06 09:04:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const { authSocket, socketServer } = require("./socketServer");
+const posts = require("./routes/posts");
+const users = require("./routes/users");
+const comments = require("./routes/comments");
+const messages = require("./routes/messages");
+const PostLike = require("./models/PostLike");
+const Post = require("./models/Post");
+
+dotenv.config();
+
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000", "https://post-it-heroku.herokuapp.com"],
+  },
+});
+
+io.use(authSocket);
+io.on("connection", (socket) => socketServer(socket));
+
+const mongoURI = 'mongodb://127.0.0.1:27017/appdb';
+mongoose.set('strictQuery', true);
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected successfully');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+httpServer.listen(4000, () => {
+  console.log("Server is listening on port 4000");
+});
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/posts", posts);
+app.use("/api/users", users);
+app.use("/api/comments", comments);
+app.use("/api/messages", messages);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+// Additional error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+module.exports = app;
+
